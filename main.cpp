630c4bb003f9082809aaf42d73cd63681498b7a9
prownd <545751287@qq.com>
2016-03-29 04:29:30 -0500

    v1
    v1

---
+++ main.cpp
@@ -0,0 +1,15 @@
+#include <QtGui/QApplication>
+#include <QTextCodec>
+#include "matrix_character.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //QTextCodec::setCodecForTr(QTextCodec::codecForName("gb18030"));
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("GBK"));
+	QTextCodec::setCodecForLocale(QTextCodec::codecForName("GBK"));
+    matrix_character matrix;
+    matrix.show();
+	qDebug()<<"wchar_t size :"<<sizeof(wchar_t);
+    return a.exec();
+}
