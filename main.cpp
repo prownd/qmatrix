842c7b684f7a63c0fe811174c1f6f19acef4d638
prownd <545751287@qq.com>
2016-06-17 11:46:21 +0800

    set gbk local

--- main.cpp
+++ main.cpp
@@ -6,11 +6,11 @@ int main(int argc, char *argv[])
 {
     QApplication a(argc, argv);
     //QTextCodec::setCodecForTr(QTextCodec::codecForName("gb18030"));
-    //set gbk
+    //set code gbk .. 
     QTextCodec::setCodecForTr(QTextCodec::codecForName("GBK"));
     QTextCodec::setCodecForLocale(QTextCodec::codecForName("GBK"));
     matrix_character matrix;
     matrix.show();
-	qDebug()<<"wchar_t size :"<<sizeof(wchar_t);
+    qDebug()<<"wchar_t size :"<<sizeof(wchar_t);
     return a.exec();
 }
