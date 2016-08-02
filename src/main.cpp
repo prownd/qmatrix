fb50fadf5f6a4f241a3f34ea7391ea885b731b76
prownd <545751287@qq.com>
2016-08-02 13:36:21 +0800

    move source file to src dir

---
+++ src/main.cpp
@@ -0,0 +1,16 @@
+#include <QtGui/QApplication>
+#include <QTextCodec>
+#include "matrix_character.h"
+
+int main(int argc, char *argv[])
+{
+    QApplication a(argc, argv);
+    //QTextCodec::setCodecForTr(QTextCodec::codecForName("gb18030"));
+    //set code gbk .. 
+    QTextCodec::setCodecForTr(QTextCodec::codecForName("GBK"));
+    QTextCodec::setCodecForLocale(QTextCodec::codecForName("GBK"));
+    matrix_character matrix;
+    matrix.show();
+    qDebug()<<"wchar_t size :"<<sizeof(wchar_t);
+    return a.exec();
+}
